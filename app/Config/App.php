976976de08643dcaf2919f2288466f80050e8184
Pancho <fmontenegro0510@gmail.com>
2026-02-26 09:48:05 -0300

    Fix url and add new sample data

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.0.30/sgc/public';
+    public string $baseURL = 'http://192.168.0.98/sgc/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
